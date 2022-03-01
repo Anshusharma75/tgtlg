27ebf8f330b308df57ee7624dc535592601b5554
Anshu Sharma <97522397+Anshusharma75@users.noreply.github.com>
2022-03-01 06:19:15 +0530

    Update custom_thumbnail.py

--- tgtlg/bot_utils/custom_thumbnail.py
+++ tgtlg/bot_utils/custom_thumbnail.py
@@ -22,20 +22,20 @@ async def save_thumb_nail(client, message):
         downloaded_file_name = await client.download_media(
             message=message.reply_to_message, file_name=download_location
         )
-        # https://stackoverflow.com/a/21669827/4723940
+         https://stackoverflow.com/a/21669827/4723940
         Image.open(downloaded_file_name).convert("RGB").save(downloaded_file_name)
         metadata = extractMetadata(createParser(downloaded_file_name))
         height = 0
         if metadata.has("height"):
             height = metadata.get("height")
-        # resize image
-        # ref: https://t.me/PyrogramChat/44663
+         resize image
+         ref: https://t.me/PyrogramChat/44663
         img = Image.open(downloaded_file_name)
-        # https://stackoverflow.com/a/37631799/4723940
-        # img.thumbnail((320, 320))
+         https://stackoverflow.com/a/37631799/4723940
+         img.thumbnail((320, 320))
         img.resize((320, height))
         img.save(thumb_image_path, "JPEG")
-        # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+         https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await ismgs.edit(
             "<b><i>Custom Thumbnail  Saved for Next Uploads</i></b>\n\n"
