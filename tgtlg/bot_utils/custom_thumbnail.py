e55b2783b767e9f72c6d4ac1c30c40d7374ecaee
Anshu Sharma <97522397+Anshusharma75@users.noreply.github.com>
2022-03-01 06:05:20 +0530

    Update custom_thumbnail.py

--- tgtlg/bot_utils/custom_thumbnail.py
+++ tgtlg/bot_utils/custom_thumbnail.py
@@ -1,10 +1,12 @@
-# Thumbnail utilities, © @AnyDLBot
+"""ThumbNail utilities, © @AnyDLBot"""
+
+
 import os
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image
-from tgtlg import DOWNLOAD_LOCATION
+from tobrot import DOWNLOAD_LOCATION
 
 
 async def save_thumb_nail(client, message):
@@ -12,7 +14,7 @@ async def save_thumb_nail(client, message):
     thumb_image_path = os.path.join(
         thumbnail_location, str(message.from_user.id) + ".jpg"
     )
-    ismgs = await message.reply_text("processing ...")
+    ismgs = await message.reply_text("<code>Processing . . . 🔄</code>")
     if message.reply_to_message is not None:
         if not os.path.isdir(thumbnail_location):
             os.makedirs(thumbnail_location)
@@ -36,21 +38,20 @@ async def save_thumb_nail(client, message):
         # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await ismgs.edit(
-            "<b>Custom video / file thumbnail saved.</b>\n"
-            + "<b>This image will be used in the upload, till /clearthumb</b>."
+            "<b>⚡<i>Custom Thumbnail 🖼 Saved for Next Uploads</i>⚡</b>\n\n"
+            + "<b><i>✅Your Photo is Set, Ready to Go ...👨‍🦯</i></b>."
         )
     else:
-        await ismgs.edit("<b>Reply to a photo to save custom thumbnail.</b>")
-
+        await ismgs.edit("<b><i>⛔Sorry⛔</i></b>\n\n" + "<b>❌ Reply with Image to Save Your Custom Thumbnail.❌</b>")
 
 async def clear_thumb_nail(client, message):
     thumbnail_location = os.path.join(DOWNLOAD_LOCATION, "thumbnails")
     thumb_image_path = os.path.join(
         thumbnail_location, str(message.from_user.id) + ".jpg"
     )
-    ismgs = await message.reply_text("processing ...")
+    ismgs = await message.reply_text("<code>Processing . . . 🔄</code>")
     if os.path.exists(thumb_image_path):
         os.remove(thumb_image_path)
-        await ismgs.edit("<b>Custom thumbnail cleared successfully.</b>")
+        await ismgs.edit("<b><i>✅Success✅</i></b>\n\n" + "<b>🖼Custom Thumbnail Cleared Successfully As Per Your Request.</b>")
     else:
-        await ismgs.edit("<b>Nothing to clear.</b>")
+        await ismgs.edit("<b><i>⛔Sorry⛔</i></b>\n\n" + "<b>❌Nothing to Clear For You❌</b>")
